93e8548b7d4e7c9626ec60fcc3ff6e361649890f
unknown <80658226+NickWirthO@users.noreply.github.com>
2021-04-07 08:44:05 -0600

    edited yaml loading for GC download test

--- download_test_GC.py
+++ download_test_GC.py
@@ -10,7 +10,7 @@ from time_efficiency import time_efficiency_decorator
 
 def load_config():
     dir_root = os.path.dirname(os.path.abspath(__file__))
-    with open(dir_root + "/config_AWS.yml", "r") as yamlfile:
+    with open(dir_root + "/config_GoogleCloud.yml", "r") as yamlfile:
         return yaml.load(yamlfile, Loader=yaml.FullLoader)
 
 def initialize_files():
